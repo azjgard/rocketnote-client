cb3ea55864a1a8766cf1232234c9b6b5d0094636
Dustin McCaffree <dustin@smartystreets.com>
2018-02-20 10:10:08 -0700

    Fixed widget to appear reliably on page load from the home page.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,26 +3,33 @@ chrome.runtime.sendMessage({
 }, () => {
 	let readyStateCheckInterval = setInterval(() => {
 		if (document.readyState === "complete") {
-			let currentVideoId = getCurrentVideoId();
-			clearInterval(readyStateCheckInterval);
-			buildWidget();
-			watchAddNoteButton();
-			watchPinButton();
-			watchKeyForInputFocus(78); 	// `n` for input focus
-			watchKeyForInputBlur(27);		// `ESC` for input blur
-			watchKeyForNoteSubmit(13); 	// `ENTER` for add note
-			watchKeyForPin(80); 				// `p` for pin
-			watchTimestampForCurrentVideo();
-			watchForPlaylist();
-			watchVideoForChanges(currentVideoId);
 
-			function watchVideoForChanges(currentVideoId) {
-				setInterval(() => {
-					if (currentVideoId !== getCurrentVideoId()) {
-						refreshWidget();
-						currentVideoId = getCurrentVideoId();
-					}
-				}, 1000);
+			if ($("#related").length) {
+				initRocketNote();
+			}
+
+			function initRocketNote() {
+				let currentVideoId = getCurrentVideoId();
+				clearInterval(readyStateCheckInterval);
+				buildWidget();
+				watchAddNoteButton();
+				watchPinButton();
+				watchKeyForInputFocus(78); 	// `n` for input focus
+				watchKeyForInputBlur(27);		// `ESC` for input blur
+				watchKeyForNoteSubmit(13); 	// `ENTER` for add note
+				watchKeyForPin(80); 				// `p` for pin
+				watchTimestampForCurrentVideo();
+				watchForPlaylist();
+				watchVideoForChanges(currentVideoId);
+
+				function watchVideoForChanges(currentVideoId) {
+					setInterval(() => {
+						if (currentVideoId !== getCurrentVideoId()) {
+							refreshWidget();
+							currentVideoId = getCurrentVideoId();
+						}
+					}, 1000);
+				}
 			}
 		}
 	}, 1000);
