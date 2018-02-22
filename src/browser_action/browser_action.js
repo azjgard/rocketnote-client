ff51c0a4fcea66b9093e57b10130b87edfdce24b
Jordin Gardner <gard.jordin@gmail.com>
2018-02-22 01:04:12 -0700

    removed onboarding comments

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -8,11 +8,6 @@ const render = name => {
 };
 
 $(document).ready($ => {
-  // It's important to understand that page actions don't retain
-  // any kind of state after they've been closed. For that reason,
-  // we need to manage all of the state in the background of the
-  // chrome extension and get the state every time that the
-  // page action is re-opened by the user.
   chrome.runtime.sendMessage({context: 'background', type: 'getState'});
 
   $('#rn_log-in').on('click', e => {
@@ -25,17 +20,10 @@ $(document).ready($ => {
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // We check the 'context' property first because we only
-  // want to subscribe to messages that are specifically sent to
-  // the popup.
   if (request.context === 'popup') {
-    // This type of request will be called every time
-    // that the popup is opened, so we can conditionally render
-    // the HTML based on whether or not the user is logged in.
     if (request.type === 'state') {
       render(request.state.userLoggedIn ? request.state.username : false);
     } else {
-      // The only other types of message will be 'login' or 'logout'
       render(request.type === 'login' ? request.data.name.givenName : false);
     }
   }
